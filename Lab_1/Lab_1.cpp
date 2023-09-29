781c2c4b4e2b07dd6198511e1821dc91380769df
Аrtem Sorokin 153503 <93012013+Axel7509@users.noreply.github.com>
2023-09-29 04:33:08 +0300

    lab_2

--- Lab_1/Lab_1.cpp
+++ Lab_1/Lab_1.cpp
@@ -9,17 +9,27 @@
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow) {
 
+	HINSTANCE hInstance = GetModuleHandle(NULL);
+	g_hHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardHookCallback, hInstance, 0);
+
 	WNDCLASS SoftwareMainClass = NewWindowClass((HBRUSH)COLOR_WINDOW, LoadCursor(NULL, IDC_HAND), hInst, LoadIcon(NULL, IDI_ERROR),
 		L"MainWndClass", SoftwareMainProcedure);
 	if (!RegisterClassW(&SoftwareMainClass)) { return -1; }
 
 	CreateWindow(L"MainWndClass", L"Lab_1 window", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 500, 250, NULL, NULL, NULL, NULL);
 
+	if (g_hHook == NULL)
+	{
+		return 1;
+	}
+
 	MSG msg = { };
 	while (GetMessage(&msg, NULL, NULL, NULL)) {
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
+
+	UnhookWindowsHookEx(g_hHook);
 
 	return 0;
 }
@@ -49,23 +59,37 @@ LRESULT CALLBACK SoftwareMainProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp
 		case OnClear:
 			SetWindowTextA(hEditControl, "");
 			break;
-		case OnRead:
-			readChars = GetWindowTextA(hEditControl, Buffer, TextBufferSize);
-			//SetWindowTextA(hStaticControl, Buffer);
-			SetWindowTextA(hStaticControl, ("Symbols read: " + std::to_string(readChars)).c_str());
-			break; 
+		case OnReadColor:
+			brushRectangle = CreateSolidBrush(
+			RGB(GetDlgItemInt(hWnd, IndexColorR, NULL, false),
+				GetDlgItemInt(hWnd, IndexColorG, NULL, false),
+				GetDlgItemInt(hWnd, IndexColorB, NULL, false)
+				));
+
+			RedrawWindow(hWnd, NULL, NULL, RDW_UPDATENOW | RDW_INVALIDATE);
+
+			break;  
 		case OnExitSoftware:
 			PostQuitMessage(0);
 			break;
 		}
 		
 		break;
+	case WM_PAINT:
+
+		BeginPaint(hWnd, &ps);
+
+		FillRect(ps.hdc, &windowRectangle, brushRectangle);
+
+		EndPaint(hWnd, &ps);
+
+		break;
 	case WM_CREATE:
 		MainWndAddMenus(hWnd);
 		MainWndAddWidgets(hWnd);
 		break;
 	case WM_DESTROY:
-		PostQuitMessage(0);
+		PostQuitMessage(0); 
 		break;
 	default: 
 		return DefWindowProc(hWnd, msg, wp, lp);
@@ -94,8 +118,34 @@ void MainWndAddWidgets(HWND hWnd) {
 
 	hStaticControl = CreateWindowA("static", "Main Window!", WS_VISIBLE | WS_CHILD | ES_CENTER, 200, 5, 100, 30, hWnd, NULL, NULL, NULL);
 
-	hEditControl = CreateWindowA("edit", "Enter text!", WS_VISIBLE | WS_CHILD | ES_MULTILINE | WS_VSCROLL, 5, 40, 470, 120, hWnd, NULL, NULL, NULL);
+	//hEditControl = CreateWindowA("edit", "Enter text!", WS_VISIBLE | WS_CHILD | ES_MULTILINE | WS_VSCROLL, 5, 40, 470, 120, hWnd, NULL, NULL, NULL);
+	windowRectangle = { 5 + 470, 40, 5, 40 + 120 };
 
-	CreateWindowA("button", "Clear", WS_VISIBLE | WS_CHILD | ES_CENTER, 130, 5, 50, 30, hWnd, (HMENU)OnClear , NULL, NULL);
-	CreateWindowA("button", "Read", WS_VISIBLE | WS_CHILD | ES_CENTER, 5, 5, 120, 30, hWnd, (HMENU)OnRead, NULL, NULL);
+	CreateWindowA("edit", "0", WS_VISIBLE | WS_CHILD | ES_MULTILINE | ES_NUMBER, 5, 170, 100, 30, hWnd, (HMENU)IndexColorR, NULL, NULL);
+	CreateWindowA("edit", "0", WS_VISIBLE | WS_CHILD | ES_MULTILINE | ES_NUMBER, 110, 170, 100, 30, hWnd, (HMENU)IndexColorG, NULL, NULL);
+	CreateWindowA("edit", "0", WS_VISIBLE | WS_CHILD | ES_MULTILINE | ES_NUMBER, 215, 170, 100, 30, hWnd, (HMENU)IndexColorB, NULL, NULL);
+
+	//CreateWindowA("button", "Clear", WS_VISIBLE | WS_CHILD | ES_CENTER, 130, 5, 50, 30, hWnd, (HMENU)OnClear , NULL, NULL);
+	CreateWindowA("button", "Set color", WS_VISIBLE | WS_CHILD | ES_CENTER, 5, 5, 120, 30, hWnd, (HMENU)OnReadColor, NULL, NULL);
+}
+
+LRESULT CALLBACK KeyboardHookCallback(int nCode, WPARAM wParam, LPARAM lParam)
+{
+
+	if (nCode == HC_ACTION && wParam == WM_KEYDOWN)
+	{
+
+		KBDLLHOOKSTRUCT* pKeyboardData = (KBDLLHOOKSTRUCT*)lParam;
+		DWORD keyCode = pKeyboardData->vkCode;
+
+		// Пример: Вывод кода нажатой клавиши в консоль
+		if (keyCode == VK_TAB)
+		{
+			// Обработка нажатия клавиши Tab
+			MessageBox(NULL, L"Tab key pressed!", L"Key Pressed", MB_OK);
+		}
+	}
+
+	// Передача управления следующему хуку в цепочке
+	return CallNextHookEx(g_hHook, nCode, wParam, lParam);
 }
--- Lab_1/SoftwareDefinitions.h
+++ Lab_1/SoftwareDefinitions.h
@@ -5,15 +5,24 @@
 #define OnMenuClicked3		3
 #define OnExitSoftware		4
 #define OnClear				5
-#define OnRead				6
+#define OnReadColor			6
 
 #define TextBufferSize		256
+#define IndexColorR			200
+#define IndexColorG			201
+#define IndexColorB			202
 char Buffer[TextBufferSize];
+
+PAINTSTRUCT ps;
+RECT windowRectangle;
+HBRUSH brushRectangle;
 
 HWND hEditControl;
 HWND hStaticControl;
-int readChars;
 
+HHOOK g_hHook = NULL;
+
+LRESULT CALLBACK KeyboardHookCallback(int nCode, WPARAM wParam, LPARAM lParam);
 LRESULT CALLBACK SoftwareMainProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp);
 WNDCLASS NewWindowClass(HBRUSH BGColor, HCURSOR Cursor, HINSTANCE hInst, HICON Icon, LPCWSTR Name, WNDPROC Procedure);
 
